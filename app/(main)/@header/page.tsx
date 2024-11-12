9f391892afecada67555366457a4dd125bca1e63
0753Ljuc <limequery@163.com>
2024-11-12 10:35:07 +0800

    refactor: file structure chagne

--- app/(main)/@header/page.tsx
+++ app/(main)/@header/page.tsx
@@ -4,18 +4,27 @@ import { useQuizContext } from "@/app/context/quizContext";
 import Github from "@/components/icons/Github";
 import ToggleLanguage from "@/components/icons/ToggleLanguage";
 import ToggleTheme from "@/components/icons/ToggleTheme";
+import { useLocale } from "@/hooks/useLocale";
 import pkg from "@/package.json";
 
 export default function Header() {
   const { quiz, quizType, quizcount } = useQuizContext();
 
+  const t = useLocale();
+
   return (
     <header>
       <div className="flex flex-row justify-between items-start py-4 ml-4 md:ml-2 mr-8 md:mr-4">
         <div className="text-sm flex flex-col md:flex-row md:gap-2">
-          <div>词库： {quiz}</div>
-          <div>挑战：{quizType}</div>
-          <div>题目数量：{quizcount}</div>
+          <div>
+            {t("home_quiz_vocabulary_title")}: {quiz}
+          </div>
+          <div>
+            {t("home_quiz_challenge_title")}: {quizType}
+          </div>
+          <div>
+            {t("home_quiz_count_title")}: {quizcount}
+          </div>
         </div>
         <div className="py-1 px-2 rounded-md bg-slate-100 border-slate-100 gap-2 flex justify-center items-center">
           <ToggleLanguage />
