72a7bb2ffbf3fd03499af80c363423296c4fb9d9
0753Ljuc <limequery@163.com>
2024-12-02 11:28:51 +0800

    chore: remove unused files

--- ai/modals/gpt.ts
+++
@@ -1,8 +0,0 @@
-import { Modal } from "./modalDefinition";
-
-export const gpt35Turbo: Modal = {
-  name: "gpt-3.5-turbo",
-  contextLength: 16_385,
-  maxInput: 16_385,
-  maxOutput: 4_096,
-};
--- ai/modals/modalDefinition.ts
+++
@@ -1,8 +0,0 @@
-export interface Modal {
-  name: string;
-  contextLength: number;
-  maxInput: number;
-  maxOutput: number;
-  inputCost?: number; // per 1000 tokens
-  outputCost?: number; // per 1000 tokens
-}
